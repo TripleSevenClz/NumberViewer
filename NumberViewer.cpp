587084cb5e51e4500a5c65ff5d006162e3d7e695
七七 柒 <triplesevenclass@outlook.com>
2024-05-21 07:27:59 +0800

    添加项目文件。

---
+++ NumberViewer.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <fstream>
+#include <Windows.h>
+#include <string>
+#include <filesystem>
+
+namespace filesystem = std::filesystem;
+
+int main()
+{
+    wchar_t runningDirectroyStr[MAX_PATH];
+    ::GetModuleFileName(nullptr, runningDirectroyStr, MAX_PATH);
+    filesystem::path runningDirectroy(runningDirectroyStr);
+    runningDirectroy = runningDirectroy.parent_path();
+
+    std::cout << "运行目录：" << runningDirectroy << std::endl;
+
+    std::ifstream trainImages(runningDirectroy / "train-images.idx3-ubyte");
+    std::ifstream trainLables(runningDirectroy / "train-labels.idx1-ubyte");
+
+
+}
